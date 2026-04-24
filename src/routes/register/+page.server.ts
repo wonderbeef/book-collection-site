16c7700b08d73e966f050e4595d8637cdafeb7c6
wonderbeef <gitboi.freeway214@passmail.com>
2026-04-24 08:58:01 -0400

    Added Supabase registration

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,51 +1,74 @@
-import type {Actions} from "./$types";
-
+import type { Actions } from "./$types";
+import { createClient } from "@supabase/supabase-js";
+import {
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY,
+} from "$env/static/public";
+import { fail, redirect } from "@sveltejs/kit";
 
 interface ReturnObject {
-    success: boolean;
-    errors: string [];
+  success: boolean;
+  errors: string[];
 }
 
 export const actions = {
-    default: async ({ request })=>{
-        const formData = await request.formData();
+  default: async ({ request }) => {
+    const formData = await request.formData();
 
-        const name = formData.get("name") as string;
-        const email = formData.get("email") as string;
-        const password = formData.get("password") as string;
-        const passwordConfirmation = formData.get("passwordConfirmation") as string;
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const passwordConfirmation = formData.get("passwordConfirmation") as string;
 
-        const returnObject: ReturnObject = {
-            success:true,
-            errors: [],
-        };
+    const returnObject: ReturnObject = {
+      success: true,
+      errors: [],
+    };
 
-        if (name.length < 3) {
-            returnObject.errors.push("Name must be at least 3 characters");
-            
-        }
-
-        if (!email.length){
-            returnObject.errors.push("Email is required for registration.");
-        }
-
-         if (!password.length){
-            returnObject.errors.push("Password cannot be blank.")
-        }
-
-        if (password !== passwordConfirmation && password.length){
-            returnObject.errors.push("Passwords must match each other");
-        }
-
-        if (returnObject.errors.length){
-            returnObject.success = false;
-            return returnObject;
-        }
-
-        //Registration flow
-
-        return returnObject;
-
-        console.log(formData);
+    if (name.length < 3) {
+      returnObject.errors.push("Name must be at least 3 characters");
     }
-}
+
+    if (!email.length) {
+      returnObject.errors.push("Email is required for registration.");
+    }
+
+    if (!password.length) {
+      returnObject.errors.push("Password cannot be blank.");
+    }
+
+    if (password !== passwordConfirmation && password.length) {
+      returnObject.errors.push("Passwords must match each other");
+    }
+
+    if (returnObject.errors.length) {
+      returnObject.success = false;
+      return returnObject;
+    }
+
+    //Registration flow
+
+    const supabase = createClient(
+      PUBLIC_SUPABASE_URL,
+      PUBLIC_SUPABASE_ANON_KEY,
+    );
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error || !data.user) {
+      console.log("There has been an error");
+      console.log(error);
+      returnObject.success = true;
+      return fail(400, returnObject);
+    }
+
+    redirect(303, "/private/dashboard");
+
+    return returnObject;
+
+    console.log(formData);
+  },
+};
